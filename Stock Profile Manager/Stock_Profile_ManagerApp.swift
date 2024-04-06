de43f3f96754035e8b7ff120962eebd23c766350
mansigarg <mansigarg60@gmail.com>
2024-04-06 07:27:44 -0700

    Initial Commit

---
+++ Stock Profile Manager/Stock_Profile_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_Profile_ManagerApp.swift
+//  Stock Profile Manager
+//
+//  Created by Mansi Garg on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Stock_Profile_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Stock Profile ManagerUITests/Stock_Profile_ManagerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Stock_Profile_ManagerUITestsLaunchTests.swift
+//  Stock Profile ManagerUITests
+//
+//  Created by Mansi Garg on 4/6/24.
+//
+
+import XCTest
+
+final class Stock_Profile_ManagerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
